e84ea4992aa6ff6718557a96596c86ba19e76fa2
EpicWolfGames <112835886+EpicWolfGames@users.noreply.github.com>
2022-09-05 12:19:56 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+function NewGameStart() {
+  const element = document.getElementById("testplay");
+  element.innerHTML = "newgametesting";
+  setTimeout(function() {
+    element.innerHTML = "";
+  }, 5000);
+}
+function LoadGameStart() {
+  const element = document.getElementById("testplay");
+  element.innerHTML = "loadtesting";
+  setTimeout(function() {
+    element.innerHTML = "";
+  }, 5000);
+}
+function CreditsStart() {
+  
+}
