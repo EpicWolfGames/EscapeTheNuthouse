0339d29051f8274111f9a0baaa0aba14cc8255ee
EpicWolfGames <112835886+EpicWolfGames@users.noreply.github.com>
2022-09-05 18:32:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 function NewGameStart() {
-  setTimeout(function() {
   document.getElementById("DivTitleScreenID").innerHTML = "";
   document.getElementById("CreditsCenterID").innerHTML = "<h1>Coming soon...</h1>";
-  }, 500);
 }
 function LoadGameStart() {
-  setTimeout(function() {
   document.getElementById("DivTitleScreenID").innerHTML = "";
   document.getElementById("CreditsCenterID").innerHTML = "<h1>Coming soon...</h1>";
-  }, 500);
 }
 function CreditsStart() {
-  setTimeout(function() {
   document.getElementById("DivTitleScreenID").innerHTML = "";
   document.getElementById("CreditsCenterID").innerHTML = "<h1>Coming soon...</h1>";
-  }, 500);
 }
-// up up down down left right left right b a start :)
