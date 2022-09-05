4b6e6b535e0505d633fa21d75a8cf93436072437
EpicWolfGames <112835886+EpicWolfGames@users.noreply.github.com>
2022-09-05 18:35:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+// when the new game button is pressed
 function NewGameStart() {
   document.getElementById("DivTitleScreenID").innerHTML = "";
   document.getElementById("CreditsCenterID").innerHTML = "<h1>Coming soon...</h1>";
 }
+// when the load game button is pressed
 function LoadGameStart() {
   document.getElementById("DivTitleScreenID").innerHTML = "";
   document.getElementById("CreditsCenterID").innerHTML = "<h1>Coming soon...</h1>";
 }
+// when the credits button is pressed
 function CreditsStart() {
   document.getElementById("DivTitleScreenID").innerHTML = "";
   document.getElementById("CreditsCenterID").innerHTML = "<h1>Coming soon...</h1>";
