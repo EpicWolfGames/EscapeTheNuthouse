4932674a9bc5faac349dec220b5584ad7d64e502
EpicWolfGames <112835886+EpicWolfGames@users.noreply.github.com>
2022-09-06 10:38:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,21 @@
 function NewGameStart() {
   setTimeout(function() {
   document.getElementById("DivTitleScreenID").innerHTML = "";
-  document.getElementById("CreditsCenterID").innerHTML = "<h1>Coming soon...</h1>";
+  document.getElementById("CreditsCenterID").innerHTML = "<h1>Coming soon...</h1> <input type='button' value='BACK'>";
   }, 500);
 }
 // when the load game button is pressed
 function LoadGameStart() {
   setTimeout(function() {
   document.getElementById("DivTitleScreenID").innerHTML = "";
-  document.getElementById("CreditsCenterID").innerHTML = "<h1>Coming soon...</h1>";
+  document.getElementById("CreditsCenterID").innerHTML = "<h1>Coming soon...</h1> <input type='button' value='BACK'>";
   }, 500);
 }
 // when the credits button is pressed
 function CreditsStart() {
   setTimeout(function() {
   document.getElementById("DivTitleScreenID").innerHTML = "";
-  document.getElementById("CreditsCenterID").innerHTML = "<h1>Coming soon...</h1>";
+  document.getElementById("CreditsCenterID").innerHTML = "<h1>Coming soon...</h1> <input type='button' value='BACK'>";
   }, 500);
 }
 // github dont break
