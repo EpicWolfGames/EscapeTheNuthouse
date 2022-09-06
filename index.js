3c1523c9aad0a6241a58aa8f1c2300215b455ca0
EpicWolfGames <112835886+EpicWolfGames@users.noreply.github.com>
2022-09-05 20:10:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // when the new game button is pressed
 function NewGameStart() {
   setTimemout(function() {
-  document.getElementById("DivTitleScreenID").innerHTML = "";
-  document.body.style.background = "black";
-  document.getElementById("LoadingScreen").innerHTML = "<h1>Loading...</h1>";
+    document.getElementById("DivTitleScreenID").innerHTML = "";
+    document.body.style.background = "black";
+    document.getElementById("LoadingScreen").innerHTML = "<h1>Loading...</h1>";
+    setTimeout(function() {
+      document.getElementById("LoadingScreen").innerHTML = "";
+      document.body.style.background = "cyan";
+    }, 5000);
   }, 500);
-  setTimeout(function() {
-    document.getElementById("LoadingScreen").innerHTML = "";
-    document.body.style.background = "cyan";
-  }, 5000);
 }
 // when the load game button is pressed
 function LoadGameStart() {
