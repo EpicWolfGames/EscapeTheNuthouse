36ea81b5ca8422dc7222148e91462c9e571f54ba
EpicWolfGames <112835886+EpicWolfGames@users.noreply.github.com>
2022-09-06 17:23:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -31,4 +31,9 @@ function BackToTitleScreen() {
   document.getElementById("DivTitleScreenID").innerHTML = "<h1>ESCAPE THE NUTHOUSE!</h1><br><input onclick='NewGameStart();' type='button' value='NEW GAME'><br><br><input onclick='LoadGameStart();' type='button' value='LOAD GAME'><br><br><input onclick='CreditsStart();' type='button' value='CREDITS'><div class='MenuChar'><div class='MenuPixels'></div></div><h5 id='EWGProductions'>AN EPICWOLF GAMES PRODUCTION</h5>";
   }, 500);
 }
-// github is being annoying ignore this comment
+// placeholder code please remember. 
+// document.getElementById("LevelsJS").innerHTML = "<script src='levels.js'></script>";
+// document.getElementById("LevelsCSS").innerHTML = "<link rel='stylesheet' href='levels.css'>";
+// document.getElementById("CutscenesJS").innerHTML = "<script src='cutscenes.js'></script>";
+// document.getElementById("CutscenesCSS").innerHTML = "<link rel='stylesheet' href='cutscenes.css'>";
+// these lines of code are used in start game
