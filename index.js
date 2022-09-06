b7784d2a4e04312aa2e62eacc3f7104bceca7c2c
EpicWolfGames <112835886+EpicWolfGames@users.noreply.github.com>
2022-09-06 11:23:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,39 @@
 function NewGameStart() {
   setTimeout(function() {
   document.getElementById("DivTitleScreenID").innerHTML = "";
-  document.getElementById("CreditsCenterID").innerHTML = "<h1>Coming soon...</h1> <input type='button' value='BACK'>";
+  document.getElementById("CreditsCenterID").innerHTML = "<h1>Coming soon...</h1> <input onclick='BackToTitleScreen();' type='button' value='BACK'>";
   }, 500);
 }
 // when the load game button is pressed
 function LoadGameStart() {
   setTimeout(function() {
   document.getElementById("DivTitleScreenID").innerHTML = "";
-  document.getElementById("CreditsCenterID").innerHTML = "<h1>Coming soon...</h1> <input type='button' value='BACK'>";
+  document.getElementById("CreditsCenterID").innerHTML = "<h1>Coming soon...</h1> <input onclick='BackToTitleScreen();' type='button' value='BACK'>";
   }, 500);
 }
 // when the credits button is pressed
 function CreditsStart() {
   setTimeout(function() {
   document.getElementById("DivTitleScreenID").innerHTML = "";
-  document.getElementById("CreditsCenterID").innerHTML = "<h1>Coming soon...</h1> <input type='button' value='BACK'>";
+  document.getElementById("CreditsCenterID").innerHTML = "<h1>Coming soon...</h1> <input onclick='BackToTitleScreen();' type='button' value='BACK'>";
   }, 500);
+}
+// takes you back to title screen
+function BackToTitleScreen() {
+  document.getElementById("CreditsCenterID").innerHTML = "";
+  document.getElementById("DivTitleScreenID").innerHTML = "
+  <h1>ESCAPE THE NUTHOUSE!</h1>
+  <br>
+  <input onclick='NewGameStart();' type='button' value='NEW GAME'>
+  <br>
+  <br>
+  <input onclick='LoadGameStart();' type='button' value='LOAD GAME'>
+  <br>
+  <br>
+  <input onclick='CreditsStart();' type='button' value='CREDITS'>
+  <div class='MenuChar'>
+    <div class='MenuPixels'></div>
+  </div>
+  <h5 id='EWGProductions'>AN EPICWOLF GAMES PRODUCTION</h5>
+  ";
 }
